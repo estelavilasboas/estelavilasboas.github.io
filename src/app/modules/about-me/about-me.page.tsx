b7cc8252b47945667911cfa87034f2f22bc69347
estelavilasboas <estelavilasboas01@gmail.com>
2022-10-13 20:08:52 -0300

    create about me page

---
+++ src/app/modules/about-me/about-me.page.tsx
@@ -0,0 +1,25 @@
+import { Body, HDisplay, Separator, Spacing } from '@atomic';
+import { Col, Grid, Row } from '@atomic/obj.grid';
+import * as React from 'react';
+import { aboutMeStrings } from './about-me.strings';
+import { BackgroundStyled, IntroductionWrapper } from './about-me.style';
+
+const strings = aboutMeStrings;
+
+export const AboutMePage: React.FC = () => {
+  return (
+    <Row noGutter>
+      <BackgroundStyled>
+        <Separator />
+          <IntroductionWrapper>
+            <HDisplay variant={'accessory'}>{strings.hello}</HDisplay>
+            <Separator size={Spacing.XSmall} />
+
+            <Body variant={'accessory3'}>{strings.introduction}</Body>
+            <Separator />
+          </IntroductionWrapper>
+        <Separator />
+      </BackgroundStyled>
+    </Row>
+  );
+};
