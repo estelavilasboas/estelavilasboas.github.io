69ade73110ba2d8f1a3a79e1b97af1a39b14300a
estelavilasboas <estelavilasboas01@gmail.com>
2022-10-29 11:16:15 -0300

    fix fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// eslint-disable-next-line import/no-unassigned-import
+import './assets/fonts/fonts.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
