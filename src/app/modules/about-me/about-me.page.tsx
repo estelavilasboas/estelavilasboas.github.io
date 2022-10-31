ceafa5902b0594d107b78ec6db693f240a5b91b3
estelavilasboas <estelavilasboas01@gmail.com>
2022-10-31 14:06:51 -0300

    create introduction section

--- src/app/modules/about-me/about-me.page.tsx
+++ src/app/modules/about-me/about-me.page.tsx
@@ -8,7 +8,6 @@ import {
 } from "../../atomic";
 import { Row } from "../../atomic/obj.grid";
 import * as React from "react";
-import { aboutMeStrings } from "./about-me.strings";
 import {
   BulletStyled,
   Divider,
@@ -17,43 +16,16 @@ import {
   TrianglePointingLeft,
   TrianglePointingRight,
 } from "./about-me.style";
-import { HorizontalImageWrapper } from "./components/atm.image/image.style";
 import { Separator } from "../../atomic/obj.box/box.component.style";
 import { Header } from "./components/mol.header/header.component";
-
-const strings = aboutMeStrings;
+import { IntroductionSection } from "./components/mol.introduction-section/introduction-section.component";
 
 export const AboutMePage: React.FC = () => {
   return (
     <Row noGutter>
       <Header />
 
-      <Hbox>
-        <Hbox.Item>
-          <Separator size={Spacing.XLarge} />
-          <HorizontalImageWrapper>
-            <img
-              alt="logo"
-              src={
-                "https://images.unsplash.com/photo-1550063873-ab792950096b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-              }
-            />
-          </HorizontalImageWrapper>
-        </Hbox.Item>
-        <Hbox.Separator size={Spacing.XXLarge} />
-
-        <Hbox.Item>
-          <Separator size={Spacing.XXLarge} />
-          <Separator size={Spacing.XLarge} />
-          <Body variant="primary">
-            {strings.introduction}
-            {strings.introduction}
-            {strings.introduction}
-          </Body>
-          <Separator size={Spacing.XXLarge} />
-        </Hbox.Item>
-        <div style={{ width: "15%" }} />
-      </Hbox>
+      <IntroductionSection />
       <Separator size={Spacing.XXLarge} />
 
       <BackgroundColor variant="accessory3">
---
+++ src/app/modules/about-me/components/mol.introduction-section/introduction-section.component.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+
+import { Hbox, Separator, Spacing, Body } from "../../../../atomic";
+import { aboutMeStrings } from "../../about-me.strings";
+import { HorizontalImageWrapper } from "../atm.image";
+
+const strings = aboutMeStrings;
+
+export const IntroductionSection: React.FC = () => (
+  <Hbox>
+    <Hbox.Item>
+      <Separator size={Spacing.XLarge} />
+      <HorizontalImageWrapper>
+        <img
+          alt="logo"
+          src={
+            "https://images.unsplash.com/photo-1550063873-ab792950096b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          }
+        />
+      </HorizontalImageWrapper>
+    </Hbox.Item>
+    <Hbox.Separator size={Spacing.XXLarge} />
+
+    <Hbox.Item>
+      <Separator size={Spacing.XXLarge} />
+      <Separator size={Spacing.XLarge} />
+      <Body variant="primary">
+        {strings.introduction}
+        {strings.introduction}
+        {strings.introduction}
+      </Body>
+      <Separator size={Spacing.XXLarge} />
+    </Hbox.Item>
+    <div style={{ width: "15%" }} />
+  </Hbox>
+);
