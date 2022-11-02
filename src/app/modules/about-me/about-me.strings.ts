7481c9298e2ac6fbaddc55ee23b85c5f7a5d7594
estelavilasboas <estelavilasboas01@gmail.com>
2022-11-02 19:46:47 -0300

    reorder

--- src/app/modules/about-me/about-me.strings.ts
+++ src/app/modules/about-me/about-me.strings.ts
@@ -4,21 +4,27 @@ export const aboutMeStrings = {
     "Eu sou a Estela. Sou desenvolvedora front-end, bacharel em Ciência da Computação com experiência na construção de websites e aplicativos mobile.",
   timeline: [
     {
-      jobTitle: "Bacharelado em Ciência da Computação",
-      employer: "Universidade Federal da Fronteira Sul (UFFS)",
-      startDate: "2016",
-      endDate: "2021",
+      jobTitle: "Desenvolvedor front-end",
+      employer: "Taqtile Brasil",
+      startDate: "outubro de 2021",
+      endDate: "atualmente",
       description:
-        "Trabalho de Conclusão de Curso: 'Geração procedural de terrenos utilizando agentes inteligentes e steering behaviors'. \nOrientado por Prof. Dr. Fernando Bevilacqua;\nAtividades e grupos:\n* Representação estudantil no Centro Acadêmico de Ciência da Computação;\n* Organização da XV Escola Regional de Banco de Dados (ERBD) e da Conferência Internacional Brasil e Argentina de Inteligência Artificial; \n* Grupo de Estudos em Ciência de Dados; \n* Grupo de Estudos de Modelagem e Impressão 3D;",
+        "- Implementar princípios de design para garantir que os sistemas sejam consistentes em múltiplos dispositivos (Web, Android e iOS); - Entender os pontos problemáticos para os usuários e validar ideias com a equipe de desenvolvimento; - Garantir a usabilidade das aplicações e corrigir os problemas; - Trabalhar no desenvolvimento de aplicações, recursos e componentes; - Revisar o app e codificação de recursos, realizando testes unitários e automatizados; - Prever inconsistências e planejar futuras atualizações junto da equipe de desenvolvimento, com foco na constante evolução do aplicativo de acordo com as necessidades do cliente; - Envolvimento em iniciativas internas como cultura empresarial, diversidade e inclusão, e estudos de acessibilidade em projetos; - Comunicação não violenta e resolução de conflitos."
+    },
+    {
+      jobTitle: "Desenvolvedor front-end",
+      jobSubtitle: "Estágio",
+      employer: "Taqtile Brasil",
+      startDate: "fevereiro de 2021",
+      endDate: "outubro de 2021",
     },
     {
       jobTitle: "Desenvolvedor de software",
-      jobSubtitle: "Estágio",
-      employer: "Universidade Federal da Fronteira Sul (UFFS)",
-      startDate: "junho de 2019",
-      endDate: "outubro de 2020",
+      employer: "PRACTICE UFFS",
+      startDate: "fevereiro de 2021",
+      endDate: "junho de 2021",
       description:
-        "Atuou no desenvolvimento de sistemas para administração interna da Universidade na Diretoria de Infraestrutura de Tecnologia da Informação (DITI) - UFFS. - Utilizou o PostgreSQL para a concretização da estrutura de dados; - Foram utilizados principalmente React, NodeJs e frameworks visuais, como AntDesign e Bulma; - Foram realizados testes nos sistemas utilizando Cypress Testing Framework; - Experiência de trabalho com arquiteturas cliente - servidor e bancos de dados; - Também analisou e desenhou novas funcionalidades para as aplicações.",
+        "- Desenvolvimento de aplicativos web e mobile, com foco em melhorias de UX e UI; - Trabalhou para garantir uma experiência positiva ao cliente; - Atuação em projetos internos e externos; - Criação de detalhes técnicos e de documentação.",
     },
     {
       jobTitle: "Líder da equipe de design",
@@ -30,26 +36,20 @@ export const aboutMeStrings = {
     },
     {
       jobTitle: "Desenvolvedor de software",
-      employer: "PRACTICE UFFS",
-      startDate: "fevereiro de 2021",
-      endDate: "junho de 2021",
-      description:
-        "- Desenvolvimento de aplicativos web e mobile, com foco em melhorias de UX e UI; - Trabalhou para garantir uma experiência positiva ao cliente; - Atuação em projetos internos e externos; - Criação de detalhes técnicos e de documentação.",
-    },
-    {
-      jobTitle: "Desenvolvedor front-end",
       jobSubtitle: "Estágio",
-      employer: "Taqtile Brasil",
-      startDate: "fevereiro de 2021",
-      endDate: "outubro de 2021",
+      employer: "Universidade Federal da Fronteira Sul (UFFS)",
+      startDate: "junho de 2019",
+      endDate: "outubro de 2020",
+      description:
+        "Atuou no desenvolvimento de sistemas para administração interna da Universidade na Diretoria de Infraestrutura de Tecnologia da Informação (DITI) - UFFS. - Utilizou o PostgreSQL para a concretização da estrutura de dados; - Foram utilizados principalmente React, NodeJs e frameworks visuais, como AntDesign e Bulma; - Foram realizados testes nos sistemas utilizando Cypress Testing Framework; - Experiência de trabalho com arquiteturas cliente - servidor e bancos de dados; - Também analisou e desenhou novas funcionalidades para as aplicações.",
     },
     {
-      jobTitle: "Desenvolvedor front-end",
-      employer: "Taqtile Brasil",
-      startDate: "outubro de 2021",
-      endDate: "atualmente",
+      jobTitle: "Bacharelado em Ciência da Computação",
+      employer: "Universidade Federal da Fronteira Sul (UFFS)",
+      startDate: "2016",
+      endDate: "2021",
       description:
-        "- Implementar princípios de design para garantir que os sistemas sejam consistentes em múltiplos dispositivos (Web, Android e iOS); - Entender os pontos problemáticos para os usuários e validar ideias com a equipe de desenvolvimento; - Garantir a usabilidade das aplicações e corrigir os problemas; - Trabalhar no desenvolvimento de aplicações, recursos e componentes; - Revisar o app e codificação de recursos, realizando testes unitários e automatizados; - Prever inconsistências e planejar futuras atualizações junto da equipe de desenvolvimento, com foco na constante evolução do aplicativo de acordo com as necessidades do cliente; - Envolvimento em iniciativas internas como cultura empresarial, diversidade e inclusão, e estudos de acessibilidade em projetos; - Comunicação não violenta e resolução de conflitos."
+        "Trabalho de Conclusão de Curso: 'Geração procedural de terrenos utilizando agentes inteligentes e steering behaviors'. \nOrientado por Prof. Dr. Fernando Bevilacqua;\nAtividades e grupos:\n* Representação estudantil no Centro Acadêmico de Ciência da Computação;\n* Organização da XV Escola Regional de Banco de Dados (ERBD) e da Conferência Internacional Brasil e Argentina de Inteligência Artificial; \n* Grupo de Estudos em Ciência de Dados; \n* Grupo de Estudos de Modelagem e Impressão 3D;",
     },
   ],
 };
